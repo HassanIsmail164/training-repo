c0a6198e3958865c19b0be4dcd9e3053b743d9f8
Hassam Ismail <haismail164@gmail.com>
2022-07-15 17:38:14 +0300

    start with Node

---
+++ index.js
@@ -0,0 +1,21 @@
+function load() {
+  y = document.getElementById("title");
+  y.innerHTML = "Nibal";
+
+  var t = 10;
+  t = 10 + 5;
+  //   alert(t);
+
+  let Roni = { name: "Roni Ghosn", year: 5 };
+  let Bahaa = { name: "Roni", year: 5 };
+  let Neam = { name: "Roni", year: 5 };
+
+  let name = "Wissam";
+  let num = 12.123456;
+  let x = Math.round(2.5);
+
+  //   alert(name.length);
+  //   alert(name.slice(0, 3));
+  // alert(x);
+  alert(Math.ceil(num));
+}
